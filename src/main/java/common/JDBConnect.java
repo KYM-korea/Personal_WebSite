21eefa7d2c3db379ce1889b1a33fd65ae7f6c969
hanga9653 <rhtqoa1@naver.com>
2022-12-08 18:08:57 +0900

    연습1

--- src/main/java/common/JDBConnect.java
+++ src/main/java/common/JDBConnect.java
@@ -1,7 +1,6 @@
 package common;
 
 import java.sql.Connection;
-<<<<<<< HEAD
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -72,34 +71,5 @@ public class JDBConnect {
 		}
 	}
 }
-=======
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
-import javax.servlet.ServletContext;
 
-public class JDBConnect {
-	public Connection con;
-	public Statement stmt;
-	public PreparedStatement psmt;
-	public ResultSet rs;
-	
-	public void close() {
-		try {
-			if(rs != null) rs.close();
-			if(psmt != null) psmt.close();
-			if(stmt != null) stmt.close();
-			if(con != null) con.close();
-			
-			System.out.println("자원 반환");
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public JDBConnect(ServletContext application) {
-		
-	}
-}
->>>>>>> branch 'main' of https://github.com/KYM-korea/Personal_WebSite.git
