4015f14d50999c80b04f8cc600ca3a8a588a069b
hanga9653 <rhtqoa1@naver.com>
2022-12-12 19:44:05 +0900

    로그인폼 완

---
+++ src/main/java/common/JDBConnect.java
@@ -0,0 +1,67 @@
+package common;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.servlet.ServletContext;
+
+public class JDBConnect {
+	public Connection con;
+	public Statement stmt;
+	public PreparedStatement psmt;
+	public ResultSet rs;
+	
+	public JDBConnect() {
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			String id = "movie";
+			String pwd = "1234";
+			con = DriverManager.getConnection(url, id, pwd);
+			System.out.println("DB 연결 성공(기본 생성자)");
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public JDBConnect(String driver, String url, String id, String pwd) {
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, id, pwd);
+			System.out.println("연결 성공");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void close() {
+		try {
+			if(rs != null) rs.close();
+			if(psmt != null) psmt.close();
+			if(stmt != null) stmt.close();
+			if(con != null) con.close();
+			
+			System.out.println("자원 반환");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public JDBConnect(ServletContext application) {
+		try {
+			String driver = application.getInitParameter("OracleDriver");
+			Class.forName(driver);
+			String url = application.getInitParameter("OracleURL");
+			String id = application.getInitParameter("OracleId");
+			String pwd = application.getInitParameter("OraclePwd");
+			con = DriverManager.getConnection(url, id, pwd);
+			System.out.println("연결 성공");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
