55575857b964b8287ec76bfc2df678925f908582
hanga9653 <rhtqoa1@naver.com>
2023-01-16 01:39:40 +0900

    1/15 글쓰기

--- src/main/java/notice/noticeWriteController.java
+++ src/main/java/notice/noticeWriteController.java
@@ -8,10 +8,21 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import MemberTable.MemberDAO;
+import MemberTable.MemberDTO;
 @WebServlet("/Notice/write.do")
 public class noticeWriteController extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session = req.getSession();
+		String id = session.getAttribute("UserId").toString();
+		MemberDAO dao = new MemberDAO();
+		MemberDTO dto = dao.selectView(id);
+		dao.close();
+		
+		req.setAttribute("dto", dto);
 		req.getRequestDispatcher("/Notice/noticeWrite.jsp").forward(req, resp);
 	}
 	@Override
@@ -19,16 +30,24 @@ public class noticeWriteController extends HttpServlet{
 		NoticeDTO dto = new NoticeDTO();
 		String title = req.getParameter("title");
 		String content = req.getParameter("content");
+		String name = req.getParameter("name");
 		String flag = req.getParameter("flag");
+		System.out.println(flag);
 		
 		dto.setTitle(title);
 		dto.setContent(content);
+		dto.setName(name);
 		dto.setFlag(flag);
 		NoticeDAO dao = new NoticeDAO();
 		int result = dao.insertNotice(dto);
 		dao.close();
 		if(result == 1) {
-			resp.sendRedirect("../Notice/List.do");
+			if(flag.equals("con")) {
+				resp.sendRedirect("../Notice/List.do?flag=con");
+			}
+			else if(flag.equals("eve")) {
+				resp.sendRedirect("../Notice/List.do?flag=eve");
+			}
 		}
 		else {
 			resp.sendRedirect("../Notice/write.do");
